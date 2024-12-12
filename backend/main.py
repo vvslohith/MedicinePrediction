b95765d79a05396d0aaf1193b6a51a0d21ae8fe2
vvslohith <vvslohith@gmail.com>
2024-12-12 21:45:51 +0530

    hello

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 from typing import List
+import os
 
 
 diseases_list = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic cholestasis', 14: 'Drug Reaction', 33: 'Peptic ulcer diseae', 1: 'AIDS', 12: 'Diabetes ', 17: 'Gastroenteritis', 6: 'Bronchial Asthma', 23: 'Hypertension ', 30: 'Migraine', 7: 'Cervical spondylosis', 32: 'Paralysis (brain hemorrhage)', 28: 'Jaundice', 29: 'Malaria', 8: 'Chicken pox', 11: 'Dengue', 37: 'Typhoid', 40: 'hepatitis A', 19: 'Hepatitis B', 20: 'Hepatitis C', 21: 'Hepatitis D', 22: 'Hepatitis E', 3: 'Alcoholic hepatitis', 36: 'Tuberculosis', 10: 'Common Cold', 34: 'Pneumonia', 13: 'Dimorphic hemmorhoids(piles)', 18: 'Heart attack', 39: 'Varicose veins', 26: 'Hypothyroidism', 24: 'Hyperthyroidism', 25: 'Hypoglycemia', 31: 'Osteoarthristis', 5: 'Arthritis', 0: '(vertigo) Paroymsal  Positional Vertigo', 2: 'Acne', 38: 'Urinary tract infection', 35: 'Psoriasis', 27: 'Impetigo'}
@@ -10,8 +11,9 @@ diseases_list = {15: 'Fungal infection', 4: 'Allergy', 16: 'GERD', 9: 'Chronic c
 # Initialize FastAPI app
 app = FastAPI()
 
+basedir=os.path.abspath(os.path.dirname(__file__))
 # Load the model
-model = joblib.load("svc.pkl")
+model = joblib.load(os.path.join(basedir, 'svc.pkl'))
 
 # Define the input schema
 class PredictionRequest(BaseModel):
